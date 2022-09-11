76c07e8c9ecad273450fe1103c8b4bdaea9241d7
Boaz <42014936+boazmenard@users.noreply.github.com>
2022-09-10 20:38:06 -0400

    switch symbolic link

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ import os, sys
 @st.experimental_singleton
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/bin/geckodriver /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver')
 
 _ = installff()
 from selenium import webdriver
